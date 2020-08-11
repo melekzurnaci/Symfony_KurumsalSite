509e97c4fc49e6e11bdd7a13dcb878ac1761bcd8
melekzurnaci <mlkzrnc@gmail.com>
2020-08-12 01:52:16 +0300

    user admin panel implementation

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -49,6 +49,8 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall.');
+
+     //   throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
